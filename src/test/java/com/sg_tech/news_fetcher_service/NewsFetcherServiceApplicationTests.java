6a0cfd72e60bd270945c5f9f5144e9d7e307ae22
shivamacshack <s.gaur@uq.edu.au>
2025-07-23 10:19:00 +1000

    Initial commit to main

---
+++ src/test/java/com/sg_tech/news_fetcher_service/NewsFetcherServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sg_tech.news_fetcher_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsFetcherServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
